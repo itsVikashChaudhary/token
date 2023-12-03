d0154fcab76c533eacf78f3a3d32e4694aa5b05b
Vikash Chaudhary <yoitschaudharyvikas99@gmail.com>
2023-12-03 22:40:37 +0530

    first

---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import {token} from "../../../declarations/token"
+
+function Faucet() {
+  const [isDisabled,setDisable] = useState(false);
+  const[buttonText,setText] = useState("Gimmi Gimmui");
+
+
+  async function handleClick(event) {
+
+    setDisable(true);
+   const result =  await token.payOut();
+   setText(result);
+  }
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free DAngela tokens here! Claim 10,000 VC coins to your account.</label>
+      <p className="trade-buttons">
+        <button 
+        id="btn-payout" 
+        onClick={handleClick}
+        disabled={isDisabled}
+        >
+          {buttonText}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
